bb2599c3cd820f2ba9e915715219b94bff76b622
victorhenriqc <vic.1601@outlook.com>
2022-11-19 13:44:14 -0300

    create a checkbox

---
+++ packages/docs/src/stories/Checkbox.stories.tsx
@@ -0,0 +1,22 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Box, Text, Checkbox, CheckboxProps } from '@victor-ui/react'
+
+export default {
+  title: 'Form/Checkbox',
+  component: Checkbox,
+  args: {},
+  decorators: [
+    (Story) => {
+      return (
+        <Box as="label" css={{ display: 'flex', flexDirection: 'row', gap: 2 }}>
+          {Story()}
+          <Text size="sm">Accept term of use</Text>
+        </Box>
+      )
+    },
+  ],
+} as Meta<CheckboxProps>
+
+export const Primary: StoryObj<CheckboxProps> = {
+  args: {},
+}
